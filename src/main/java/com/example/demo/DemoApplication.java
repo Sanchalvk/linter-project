8622c48ab6bffc5b38054c3605cecea7881877ab
Sanchalvk <khedkarsanchal86@gmail.com>
2025-03-27 22:28:50 -1100

    my pmd and argu error

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args)
+ 	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
 		String message = "Hello, world!"; // Line is too long, use single quotes j=
 		if (args.length > 0) {
             System.out.println(args[0]); // PMD error: Hardcoded value in condition
+			int unusedVariable = 10; // This variable is never use
         }
         
         long timeLimit = 1000L;   //long 
