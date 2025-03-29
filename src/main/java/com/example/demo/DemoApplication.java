0473beeef14cb73179d5a68e8da9c8ffc6ee4cf0
Sanchalvk <khedkarsanchal86@gmail.com>
2025-03-29 05:03:48 -1100

    123455

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,32 +8,7 @@ public class DemoApplication {
  	public static void main(String[] args) { 
 		SpringApplication.run(DemoApplication.class, args);
 		String message = "Hello, world!"; // Line is too long, use single quotes j=
-		if (args.length > 0) {
-            System.out.println(args[0]); // PMD error: Hardcoded value in condition
-			
-        }
-		double x=10/2;
-		System.out.println0('Q');
-
-		System.out.println(x) ;
-
-		double y=2+8/2;
-		System.out.println(x); 
-
-		
-
-		int unusedVariable = 10; // This variable is never use
-		
-		
-		try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            // Empty catch block ndwqhh
-        }
-        
-        long timeLimit = 1000L;   //long 
-
-
+	
 	}
 
 }
