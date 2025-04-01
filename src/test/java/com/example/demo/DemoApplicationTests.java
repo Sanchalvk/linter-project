31f8bd9d63c7cf0c8aee152a4cc7118d5dbbbd20
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-03-31 22:26:54 -1100

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+public class DemoApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args); // Error: indentation level is wrong
+    }
+    
+    public static void exampleMethod() {
+    int x = 0; // Error: variable declaration should follow naming conventions.
+    System.out.println(x);
+    }
 
 }
