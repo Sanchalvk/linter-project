83de389372ecb0bfcd5090e3b32dd47faa4003ea
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-03-31 22:43:40 -1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
  	public static void main(String[] args) { 
-		SpringApplication.run(DemoApplication.class, args);
-		String message = "Hello, world!"; // Line is too long, use single quotes j=
+		SpringApplication.run(DemoApplication.class, args)
+		String message = "Hello, world!"; // Line is too long, use single quotes j
 	
 	}
 
