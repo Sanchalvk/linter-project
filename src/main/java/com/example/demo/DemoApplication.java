0eb88b20478a014075542b4349921c18d49ca33c
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-04-01 19:39:50 -1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,17 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import java.util.List;  // Unused import - Removed
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        String message = "Hello, world!"; // Removed incorrect comment about single quotes
+        String message = "Hello, world!";
     }
 
-    public static class MyClass { // Made inner class static
-        public void myMethod() { // Removed trailing characters from comment
+    public static class MyClass {
+        public void myMethod() {
             System.out.println("Hello");
         }
     }
