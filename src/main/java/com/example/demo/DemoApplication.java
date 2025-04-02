fa1d4c6d0eb1d503b214762e64578979749c72db
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-04-01 19:50:34 -1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,12 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        String message = "Hello, world!";
+        String messageTooLongAndUnnecessary = "This is a very long and unnecessary variable name to trigger a PMD rule.";
+        System.out.println("Hello, world!");
     }
 
     public static class MyClass {
-        public void myMethod() {
-            System.out.println("Hello");
+        public void doSomethingVeryComplicated() { // Method name is quite long
+            int a = 10;
+            int b = 20;
+            int result = a + b;
+            System.out.println("Result: " + result);
         }
     }
 }
